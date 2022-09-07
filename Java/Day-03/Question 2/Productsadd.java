777faaafb8ef96c7052f3404631b7dd5152e1fb6
thanush <thanush.pk@sonata-software.com>
2022-09-07 09:36:21 +0530

    file changes

--- Java/Day-03/Question 2/Productsadd.java
+++ Java/Day-03/Question 2/Productsadd.java
@@ -6,11 +6,16 @@ import java.util.List;
 public class Productsadd {
 	public List<Products>addProducts(){
 		List<Products> prd=new LinkedList<Products>();
-		prd.add(new Products(101,"Pen",-1));
+		prd.add(new Products(101,"Pen",150));
 		prd.add(new Products(102,"Pencil",350));
 		prd.add(new Products (103,"Eraser",550));
 		prd.add( new Products(104,"writing pad",660));
 		prd.add(new Products(105,"A4 paper",230));
+		prd.forEach((Products p1)->{
+			if (p1.getPrdPrice() <0) 
+				throw new RuntimeException("Price caanot be negative");
+			
+		});
 		return prd;
 	}
 
