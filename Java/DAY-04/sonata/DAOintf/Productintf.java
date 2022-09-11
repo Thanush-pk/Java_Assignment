c70009ad967e3f88b68f35aa2b815a7a48b29e17
Thanush <thanushpk16@gmail.com>
2022-09-11 23:18:36 +0530

    Add files via upload

---
+++ Java/DAY-04/sonata/DAOintf/Productintf.java
@@ -0,0 +1,10 @@
+package com.sonata.DAOintf;
+
+import java.util.List;
+
+import com.sonata.Model.Product;
+
+public interface Productintf {
+	//public int save(Object object);
+	public List<Product> getdata();
+}
